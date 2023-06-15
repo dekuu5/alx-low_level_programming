34bc26615be42378f29fb9173a950f039b2eb23f
dekuu5 <deku332211@gmail.com.com>
2023-06-15 14:01:57 -0700

    efqfqbfiuqbf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*
- * this fuction prints the text blewo using prinf
- * */
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
